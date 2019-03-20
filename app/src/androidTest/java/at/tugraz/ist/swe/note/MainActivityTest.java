d04ce3210ef3beda8cb404a1431b222c21057509
Tina Promitzer <tina.promitzer@student.tugraz.at>
2019-03-20 12:54:14 +0100

    NOTE-001 implemented tests for note overview [PT,GA]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/note/MainActivityTest.java
@@ -0,0 +1,52 @@
+package at.tugraz.ist.swe.note;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
+import android.app.Activity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Test
+    public void checkCreateNoteButtonVisibility() {
+        onView(withId(R.id.createNoteButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkIfCreateButtonIsClickable() {
+        onView(withId(R.id.createNoteButton)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void checkNotesListViewVisibility() {
+        onView(withId(R.id.notesList)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkIfNotesAreClickable() {
+        onData(anything()).inAdapterView(withId(R.id.notesList)).atPosition(0).check(isClickable());
+    }
+}
