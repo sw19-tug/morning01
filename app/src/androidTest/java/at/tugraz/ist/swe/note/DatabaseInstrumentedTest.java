4babca829f9a92013e22ec3240facf719894ded2
alexandru.agape <alexandru.agape@student.tugraz.at>
2019-03-24 17:14:41 +0100

    NOTE-011 aded database helper class [AA,MF]

--- app/src/androidTest/java/at/tugraz/ist/swe/note/DatabaseInstrumentedTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/note/DatabaseInstrumentedTest.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import at.tugraz.ist.swe.note.database.DatabaseHelper;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
