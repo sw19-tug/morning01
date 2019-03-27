763112eb2469a10bc1fe2439efbead66a97dfe3d
Franz Mandl <franz.mandl@student.tugraz.at>
2019-03-27 09:32:32 +0100

    NOTE-012 add note insert test [KD,MF]

--- app/src/androidTest/java/at/tugraz/ist/swe/note/DatabaseInstrumentedTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/note/DatabaseInstrumentedTest.java
@@ -1,5 +1,6 @@
 package at.tugraz.ist.swe.note;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
@@ -12,6 +13,7 @@ import at.tugraz.ist.swe.note.database.DatabaseHelper;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -38,5 +40,28 @@ public class DatabaseInstrumentedTest {
     public void testNoteTable() {
         SQLiteDatabase database = databaseHelper.getReadableDatabase();
         database.query("note", null, null, new String[]{}, null, null, null);
+    }
+
+    @Test
+    public void testNoteInsert() {
+        Note note = new Note("title1", "content1", 1);
+        note.save();
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        String selection = DatabaseHelper.NOTE_COLUMN_TITLE + " = ? AND " + DatabaseHelper.NOTE_COLUMN_CONTENT + " = ? AND " + DatabaseHelper.NOTE_COLUMN_PINNED + " = ?";
+
+        String[] selectionArgs = {"title1", "content1", "1"};
+
+        Cursor cursor = database.query(
+                DatabaseHelper.NOTE_TABLE_NAME,
+                null,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+
+        assertTrue(cursor.getCount() > 0);
     }
 }
