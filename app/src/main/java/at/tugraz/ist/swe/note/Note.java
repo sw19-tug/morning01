c1055b3f80f997354103fd01c8d43af0426590fb
Franz Mandl <franz.mandl@student.tugraz.at>
2019-03-28 11:15:28 +0100

    NOTE-013 remove save method in Node class and add setters [KD,MF]

--- app/src/main/java/at/tugraz/ist/swe/note/Note.java
+++ app/src/main/java/at/tugraz/ist/swe/note/Note.java
@@ -1,25 +1,20 @@
 package at.tugraz.ist.swe.note;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import  at.tugraz.ist.swe.note.database.DatabaseHelper;
 
 public class Note {
+    public static long NOT_CREATED_YET = -1;
+
+    private long id;
     private String title;
     private String content;
     private Date createdDate;
     private  boolean removed;
     private int pinned;
     private Date changedDate;
-    public static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public Note(){
+        this.id = NOT_CREATED_YET;
         this.title = "";
         this.content = "";
         this.createdDate = new Date();
@@ -29,15 +24,21 @@ public class Note {
     }
 
     public Note(String title, String content, int pinned){
+        this.id = NOT_CREATED_YET;
         this.title = title;
         this.content = content;
         this.createdDate = new Date();
         this.removed = false;
         this.pinned = pinned;
         this.changedDate = new Date();
-
     }
 
+    public boolean exists() {
+        return id != NOT_CREATED_YET;
+    }
+    public long getId() {
+        return id;
+    }
     public String getTitle() {
         return title;
     }
@@ -57,23 +58,31 @@ public class Note {
         return changedDate;
     }
 
-
-    static private String convertDateToString(Date date) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault());
-        return dateFormat.format(date);
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public boolean save(Context context){
-        SQLiteDatabase database = new DatabaseHelper(context).getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(DatabaseHelper.NOTE_COLUMN_TITLE, title);
-        values.put(DatabaseHelper.NOTE_COLUMN_CONTENT, content);
-        values.put(DatabaseHelper.NOTE_COLUMN_PINNED, pinned);
-        values.put(DatabaseHelper.NOTE_COLUMN_CREATED_DATE, convertDateToString(createdDate));
-        values.put(DatabaseHelper.NOTE_COLUMN_CHANGED_DATE, convertDateToString(changedDate));
-        values.put(DatabaseHelper.NOTE_COLUMN_REMOVED, removed);
-        database.insert(DatabaseHelper.NOTE_TABLE_NAME, null, values);
-        return true;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setRemoved(boolean removed) {
+        this.removed = removed;
+    }
+
+    public void setPinned(int pinned) {
+        this.pinned = pinned;
+    }
+
+    public void setChangedDate(Date changedDate) {
+        this.changedDate = changedDate;
+    }
 }
