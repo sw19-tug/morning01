a2f7d37a44b5a85cc9414782932f14b8be012cf5
Franz Mandl <franz.mandl@student.tugraz.at>
2019-03-28 11:37:18 +0100

    NOTE-013 implement find note by id [MF,DK]

--- app/src/main/java/at/tugraz/ist/swe/note/NoteStorage.java
+++ app/src/main/java/at/tugraz/ist/swe/note/NoteStorage.java
@@ -1,11 +1,13 @@
 package at.tugraz.ist.swe.note;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import at.tugraz.ist.swe.note.database.DatabaseHelper;
+import at.tugraz.ist.swe.note.database.NotFoundException;
 
 public class NoteStorage {
     public static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
@@ -21,6 +23,14 @@ public class NoteStorage {
         return DATE_FORMAT.format(date);
     }
 
+    static private Date convertStringToDate(String date) {
+        try {
+            return DATE_FORMAT.parse(date);
+        } catch(java.text.ParseException e) {
+            return null;
+        }
+    }
+
     /**
      * @return Returns true if note was inserted, false otherwise.
      */
@@ -33,6 +43,27 @@ public class NoteStorage {
             return true;
 
         }
+    }
+
+    public Note findById(long id) throws NotFoundException {
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+        String selection = DatabaseHelper.NOTE_COLUMN_ID + " = ?";
+        String[] selectionArgs = {String.valueOf(id)};
+        Cursor cursor = database.query(DatabaseHelper.NOTE_TABLE_NAME, null, selection, selectionArgs, null, null, null);
+        if(cursor.getCount() != 1) {
+            throw new NotFoundException(id);
+        }
+        Note note = new Note();
+        cursor.moveToFirst();
+        note.setId(cursor.getLong(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_ID)));
+        note.setTitle(cursor.getString(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_TITLE)));
+        note.setContent(cursor.getString(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_CONTENT)));
+        note.setCreatedDate(convertStringToDate(cursor.getString(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_CREATED_DATE))));
+        note.setChangedDate(convertStringToDate(cursor.getString(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_CHANGED_DATE))));
+        note.setPinned(cursor.getInt(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_PINNED)));
+        note.setRemoved(cursor.getInt(cursor.getColumnIndex(DatabaseHelper.NOTE_COLUMN_REMOVED)) > 0);
+        cursor.close();
+        return note;
     }
 
     private ContentValues getContentValues(Note note) {
